d078e4c0dd22cddefa108062d626261c40680315
Yari-Ros <2118088@talnet.nl>
2019-11-15 12:14:35 +0100

    Uitwerking van de exercise

---
+++ groet.php
@@ -0,0 +1,8 @@
+<?php
+
+$hello =readline ( "Tell me, young one. What is your name?");
+
+echo "What an unusual name you have.." .$hello;
+echo " , it seems that still have a lot to learn";
+$answer =readline ("How did you get in contact with me?");
+echo "Is that so..? I must take my leave, we shall discuss this some other time.";
